0b11a64d1d9270c46573432b076cfb666705b110
Katiuska Alicea <katiuskaalicea@gmail.com>
2024-04-24 22:47:29 -0700

    Add react-loader-spinner dependency and implement loading state in CardsWrapper.jsx

--- src/components/CardsWrapper.jsx
+++ src/components/CardsWrapper.jsx
@@ -1,26 +1,42 @@
-import React, { useState, useEffect } from 'react'
-import Card from './Card'
-import axios from 'axios'
+import React, { useState, useEffect } from 'react';
+import Card from './Card';
+import axios from 'axios';
+import { InfinitySpin } from 'react-loader-spinner';
 
 const CardsWrapper = () => {
-    const [cards, setCards] = useState([])
+    const [cards, setCards] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        axios.get("https://botw-compendium.herokuapp.com/api/v3/compendium/all")
+        axios
+            .get('https://botw-compendium.herokuapp.com/api/v3/compendium/all')
             .then((res) => {
-                setCards(res.data.data)
-            }).catch((err) => console.log(err))
-    }, [])
+                setCards(res.data.data);
+                setLoading(false);
+            })
+            .catch((err) => console.log(err));
+    }, []);
 
     return (
         <div className='sm:container sm:w-max md:container md:w-max'>
-            {cards.map((card) => {
-                return (
-                    <Card card={card} key={card.id} />
-                )
-            })}
+            {loading ? (
+                <div className='sm:container sm:w-max md:container md:w-max'>
+                    <InfinitySpin />
+                </div>
+            ) : (
+                <>
+                    {cards.map((card) => {
+                        return (
+                            <Card
+                                card={card}
+                                key={card.id}
+                            />
+                        );
+                    })}
+                </>
+            )}
         </div>
-    )
-}
+    );
+};
 
-export default CardsWrapper
+export default CardsWrapper;
