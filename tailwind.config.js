c212e74454c764d68f7f68f964a06d335d33469a
Katiuska Alicea <katiuskaalicea@gmail.com>
2024-04-24 21:36:44 -0700

    Add responsive screen sizes to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,16 @@ export default {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     extend: {},
+     screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    }
   },
+
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["emerald"],
+    themes: ["forest"],
   },
 }
